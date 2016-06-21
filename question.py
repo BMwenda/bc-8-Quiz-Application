0d08a9a92714e4598b6a4c84e89118513a1e8b66
Morris Kimani <mzeemorris@gmail.com>
2016-06-21 15:06:22 +0300

    Modifies the question class by removing the question_type instance variable

--- question.py
+++ question.py
@@ -1,8 +1,11 @@
 class Question(object):
-	"""This class models question objects"""
-	def __init__(self, question_text, answer, choices):
-		"""This method accepts the following mandatory arguments:
-		question_type ->
+    """This class models question objects"""
+    def __init__(self, question_text, answer, choices):
+		"""
+		This method accepts the following mandatory arguments:
+		question_text: The question itself
+		answer: The answer to the question
+		choices: A list containing the various choices
 		"""
 		self.question_text = question_text
 		self.answer = answer
@@ -11,7 +14,4 @@ class Question(object):
 	def grade(self, user_answer):
 		if self.answer.lower() == user_answer.lower():
 			return True
-		return False
-
-
-
+        return False
