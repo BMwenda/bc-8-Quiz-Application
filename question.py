5be1c07ee1255bf4974e57837c7c7296b380cba7
Morris Kimani <mzeemorris@gmail.com>
2016-06-22 10:30:00 +0300

    Adds the method 'to_string' that returns a string containing the question and its choices

--- question.py
+++ question.py
@@ -1,17 +1,28 @@
 class Question(object):
-    """This class models question objects"""
+    """This class models question objects."""
+
     def __init__(self, question_text, answer, choices):
-		"""
-		This method accepts the following mandatory arguments:
-		question_text: The question itself
-		answer: The answer to the question
-		choices: A list containing the various choices
-		"""
-		self.question_text = question_text
-		self.answer = answer
-		self.choices = choices
+        """
+        Create a new Question object.
+
+        The method accepts:
+            question_text: The question itself
+            answer: The answer to the question
+            choices: A list containing the various choices
+        """
+        self.question_text = question_text
+        self.answer = answer
+        self.choices = choices
 
     def grade(self, user_answer):
-		if self.answer.lower() == user_answer.lower():
-			return True
-		return False
+        """Check whether answer provided is correct."""
+        if self.answer.lower() == user_answer.lower():
+            return True
+        return False
+
+    def to_string(self):
+        """Return a string containing the question and its choices."""
+        final_string = self.question_text + "\n"
+        for key in sorted(self.choices.keys()):
+            final_string += key + ". " + self.choices[key] + "\n"
+        return final_string
